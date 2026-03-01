add4ce37242f13e6660b78f1894f7db75c42ef26
Viqo07 <vicomus19@gmail.com>
2026-02-28 19:55:26 -0600

    Add files via upload

---
+++ Exceptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MedScheduler
+{
+	public class DoubleBookingException : Exception
+	{
+		public DoubleBookingException() { }
+		public DoubleBookingException(string message) : base(message) { }
+		public DoubleBookingException(string message, Exception innerException)
+			: base(message, innerException) { }
+	}
+
+	public class InvalidAppointmentTimeException : Exception
+	{
+		public InvalidAppointmentTimeException() { }
+		public InvalidAppointmentTimeException(string message) : base(message) { }
+		public InvalidAppointmentTimeException(string message, Exception innerException)
+			: base(message, innerException) { }
+	}
+}
